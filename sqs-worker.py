fb4d2f424f99543fde18c1ba3d3fb316487eacf8
bork1n <24889495+bork1n@users.noreply.github.com>
2018-09-15 13:44:15 +0300

    refactor

--- sqs-worker.py
+++ sqs-worker.py
@@ -8,8 +8,56 @@ from ihelper.Fetcher import Fetcher
 import ihelper.Storage
 from ihelper.User import User
 from SQSClient import SQSClient
+from time import sleep
 
 CONFIG_FILE = "settings.json"
+
+
+def update_followers(payload):
+    user = User(id=payload['user_id'],
+                fetcher=fetcher, storage=storage, logger=logger)
+    prev_ts = user.last_followers_ts()
+    followers = user.update_followers(ts=int(start_time))
+
+    if followers:
+        process_request = {
+            "command": "process_diff",
+            "payload": {
+                "user_id": payload['user_id'],
+                "from": prev_ts,
+                "to": int(start_time)
+            }
+        }
+        sqs.send(process_request)
+    else:
+        logger.warning("followers are empty. Something wrong?")
+
+
+def process_diff(payload):
+    user = User(id=payload['user_id'],
+                fetcher=fetcher, storage=storage, logger=logger)
+    r = user.calc_diff(payload['from'], payload['to'])
+    for i in 'added', 'removed':
+        size = len(r[i].items())
+        counter = 0
+        for id, item in r[i].items():
+            if counter % 30 == 0:
+                logger.info("%s %d/%d", i, counter, size)
+            counter += 1
+
+            follow_update = {
+                "producer": payload['user_id'],
+                "action": 1 if i is 'added' else 0
+            }
+            logger.info("saving: follows/%s ts=%s val=%s",
+                        item['id'], payload['to'], follow_update)
+
+            storage.save_data(
+                key='follows/' + item['id'], value=follow_update, ts=payload['to'])
+
+            details = User(id=item['id'], fetcher=fetcher,
+                           storage=storage, logger=logger).get_user_info(item)
+            sleep(0.01)
 
 
 with open(CONFIG_FILE) as f:
@@ -49,7 +97,7 @@ while True:
     start_time = time.time()
 
     try:
-        payload = json.loads(msg[0].body)
+        request = json.loads(msg[0].body)
     except AttributeError as e:
         logger.info("msg[0] has no body attribute. WTF? removing.")
         msg[0].delete()
@@ -61,22 +109,17 @@ while True:
         msg[0].delete()
         continue
 
-    logger.info('got request for %s', payload)
-    user = User(id=payload['update_followers'],
-                fetcher=fetcher, storage=storage, logger=logger)
+    command = request.get('command')
+    payload = request.get('payload')
+    logger.info("got request for '%s' payload %s", command, payload)
 
-    old = user.get_followers()
-    followers = user.update_followers()
+    if command == 'update_followers':
+        update_followers(payload)
+    elif command == 'process_diff':
+        process_diff(payload)
+    else:
+        logger.warning('invalid request %s', request)
 
-    if not followers:
-        logger.info("followers are empty. Something wrong?")
-        continue
-
-    r = user.calc_diff(old, followers)
-    for i in 'added', 'removed':
-        for id, item in r[i].items():
-            details = User(id=item['id'], fetcher=fetcher,
-                           storage=storage, logger=logger).get_user_info(item)
     msg[0].delete()
     elapsed_time = int(time.time() - start_time)
     logger.info("request finished in {} seconds".format(elapsed_time))
